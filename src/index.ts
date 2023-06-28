547d8a3205cf0a8d715ad1789e560e17c4424191
Nattapong <banker.z412@gmail.com>
2023-06-29 00:04:45 +0700

    create public directory to serve static files

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import jwtValidate from "./middleware/jwtValidate";
-
+import path from "path";
+import multer from "multer";
 const auth = require("./routes/auth");
 const posts = require("./routes/posts");
+
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/public", express.static(path.resolve(__dirname, "../public")));
 
 app.use("/api/auth", auth);
 app.use("/api/posts", posts);
